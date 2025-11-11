2ad1918e435f7a8ad1dcf9b3add060beb26ca4ae
Paulo Barbosa <paulobarbosah@hotmail.com>
2025-11-11 09:57:08 -0300

    feat: adicionar configuração do ESLint e Prettier, incluindo arquivos de configuração e regras personalizadas

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
+export default defineConfig(() => {
+  return {
       server: {
         port: 3000,
         host: '0.0.0.0',
@@ -16,7 +15,7 @@ export default defineConfig(({ mode }) => {
       // backend/proxy — não os exponha no front-end.
       resolve: {
         alias: {
-          '@': path.resolve(__dirname, '.'),
+          '@': path.resolve(__dirname, 'src'),
         }
       }
     };
